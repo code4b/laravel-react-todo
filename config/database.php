d9be9e02f57c20552d1ab8fe678a5ae42a385a4c
code4b <44916878+code4b@users.noreply.github.com>
2018-12-14 05:42:25 +0530

    db con

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,11 +56,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'ec2-23-21-188-236.compute-1.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'd6610dos7uu689'),
+            'username' => env('DB_USERNAME', 'uhohplpzzcziik'),
+            'password' => env('DB_PASSWORD', 'eab2944be631ca05e10cc57708a7d531cef8a03f019e1546fed1cd428edab6af'),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
